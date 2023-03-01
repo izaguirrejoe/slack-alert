eebddb8dd802f8a0caab2f6602c848e826af6a03
Joseph Izaguirre <izaguirrejoe@gmail.com>
2023-03-01 15:02:10 -0800

    Done

---
+++ app/controllers/alerts_controller.rb
@@ -0,0 +1,17 @@
+class AlertsController < ApplicationController
+
+  def create
+    notifier = Slack::Notifier.new ENV[SLACK_WEBHOOK] 
+    record_type = params["Type"]
+    email = params["Email"]
+    description = params["Description"]
+    if record_type == "SpamNotification"
+      message = "*Alert! Email flagged as spam.*\n"
+      message += "*Email:* #{email}\n"
+      message += "*Description:* #{description}\n"
+      notifier.ping  message
+    end
+    render status: :ok
+  end
+
+end
